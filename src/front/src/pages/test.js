14d98e4b89d9b5878d4728674ab0cfa39e6af872
vardy <jarred.vardy@gmail.com>
2020-12-17 21:40:16 +1000

    Fix spacing

--- src/front/src/pages/test.js
+++ src/front/src/pages/test.js
@@ -1,6 +1,7 @@
 import React, {useEffect, useState} from "react";
 import axios from "axios"
 import PrimaryLayout from "../layouts/primaryLayout";
+
 import Spinner from "../components/Spinner";
 
 const Test = () => {
