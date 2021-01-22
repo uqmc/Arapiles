6e0fc9bd4843bd76e8c03f44f3396fb0defd0fb2
Ray Clark <raynichc@users.noreply.github.com>
2021-01-22 16:35:11 +1100

    Created basic login page

---
+++ src/src/pages/login.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import PrimaryLayout from "../layouts/primaryLayout";
+import Login from "../components/Login";
+
+//Test Login page using Login component
+const LOGIN = () => {
+    return (
+        <PrimaryLayout>
+            <Login />
+        </PrimaryLayout>
+    );
+};
+
+export default LOGIN;
