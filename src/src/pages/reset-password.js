b35165811f6bcdebdff3adf4149b8c5284aa3af6
Ray Clark <raynichc@users.noreply.github.com>
2021-01-26 14:22:42 +1100

    Created basic reset password page

---
+++ src/src/pages/reset-password.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import PrimaryLayout from "../layouts/primaryLayout";
+
+//Basic Reset Password page
+const RESET_PASSWORD = () => {
+    return (
+        <PrimaryLayout>
+        </PrimaryLayout>
+    );
+};
+
+export default RESET_PASSWORD;
