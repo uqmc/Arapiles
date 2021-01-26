420bf52bcdb98615995bd3298bf97495cd1ad737
Ray Clark <raynichc@users.noreply.github.com>
2021-01-26 14:22:34 +1100

    Created basic forgot-password page

---
+++ src/src/pages/forgot-password.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import PrimaryLayout from "../layouts/primaryLayout";
+
+//Basic Reset Password page
+const FORGOT_PASSWORD = () => {
+    return (
+        <PrimaryLayout>
+        </PrimaryLayout>
+    );
+};
+
+export default FORGOT_PASSWORD;
