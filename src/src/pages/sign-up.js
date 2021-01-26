ca0b3d9dc5f8ee93074000bd021296fa4b6f6ebc
Ray Clark <raynichc@users.noreply.github.com>
2021-01-26 14:22:50 +1100

    Created basic sign up page

---
+++ src/src/pages/sign-up.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import PrimaryLayout from "../layouts/primaryLayout";
+
+//Basic Reset Password page
+const SIGN_UP = () => {
+    return (
+        <PrimaryLayout>
+        </PrimaryLayout>
+    );
+};
+
+export default SIGN_UP;
