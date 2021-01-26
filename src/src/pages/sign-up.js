9cbf5a7d30fcd1962610db0863327b4df49e9766
Ray Clark <raynichc@users.noreply.github.com>
2021-01-26 14:44:25 +1100

    Implemented component

--- src/src/pages/sign-up.js
+++ src/src/pages/sign-up.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import PrimaryLayout from "../layouts/primaryLayout";
+import SignUp from "../components/SignUp";
 
 //Basic Reset Password page
 const SIGN_UP = () => {
     return (
         <PrimaryLayout>
+        	<SignUp />
         </PrimaryLayout>
     );
 };
