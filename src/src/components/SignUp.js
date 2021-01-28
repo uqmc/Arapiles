3784ccc5de92d299b4376ec79c9d0ef64fdbccff
Ray Clark <raynichc@users.noreply.github.com>
2021-01-28 14:35:33 +1100

    (WIP) Revamp of Sign up form

--- src/src/components/SignUp.js
+++ src/src/components/SignUp.js
@@ -1,5 +1,7 @@
 import React from "react"
 
+import { Formik, Field, Form, ErrorMessage } from "formik"
+
 //navigate used to redirect
 import { navigate } from "gatsby"
 
@@ -13,9 +15,47 @@ class SignUp extends React.Component {
         super(props);
 
         this.state = {
+            genders: [
+                {label: "Male", value: "Male"},
+                {label: "Female", value: "Female"},
+                {label: "Not Stated", value: "Not_Stated"}
+            ],
+
+            studentStatuses: [
+                {label: "Not UQ", value: "Not_UQ"},
+                {label: "Domestic", value: "Domestic"},
+                {label: "International", value: "International"},
+                {label: "Exchange", value: "Exchange"},
+                {label: "Staff", value: "Staff"}
+            ],
+
+            //Register information
             username: "",
             email: "",
             password: "",
+
+            //Personal information
+            nameFirst: "",
+            nameLast: "",
+            gender: "",
+            phoneNumber: "",
+            phoneType: "",
+            dateOfBirth: "",
+            address: "",
+            postcode: "",
+            studentStauts: "",
+            studentNumber: "",
+
+            //Emergency Information
+            contactName: "",
+            contactPhoneNumber: "",
+            contactPhoneType: "",
+            contactAddress: "",
+
+            //Membership Information
+            waiver: false,
+            membershipAgreement: false,
+
             error: ""
         }
     }
@@ -25,11 +65,18 @@ class SignUp extends React.Component {
         this.setState({error: error});
     }
 
-    //Function to update state when either field is changed
-    handleUpdate = (event) => {
-        this.setState({
-          [event.target.name]: event.target.value,
-      })
+    handleValidate = (values) => {
+        const errors = {}
+
+        if (!values.nameFirst) {
+            errors.nameFirst = "Required"
+        }
+
+        if (!values.nameLast) {
+            errors.nameLast = "Required"
+        }
+
+        return errors
     }
 
     //Function to handle login form submission
@@ -51,24 +98,77 @@ class SignUp extends React.Component {
     //Simple Sign Up form
     render() {
         return (
-            <>
-                <h1>Sign Up</h1>
-                <span style={{color: "red"}}>{this.state["error"]}</span>
-                <form
-                    method="post"
-                    onSubmit={event => {
-                        this.handleSubmit(event)
-                    }}
-                >
-                    <input type="text" name="username" placeholder="Username" onChange={this.handleUpdate} />
+            <Formik
+                initialValues={{
+                    nameFirst: ""
+                }}
+                validate={this.handleValidate}
+                onSubmit={this.handleSubmit}
+            >
+                <Form>
+
+                    <label htmlFor="nameFirst">First Name</label>
+                    <Field name="nameFirst" />
+                    <ErrorMessage name="nameFirst" />
                     <br />
-                    <input type="text" name="email" placeholder="Email" onChange={this.handleUpdate} />
+
+                    <label htmlFor="nameLast">Last Name</label>
+                    <Field name="nameLast" />
+                    <ErrorMessage name="nameLast" />
                     <br />
-                    <input type="password" name="password" placeholder="Password" onChange={this.handleUpdate} />
+
+                    <label htmlFor="gender">Gender</label>
+                    <Field name="gender" as="select">
+                        <option defaultValue>Please Select...</option>
+                        {
+                            this.state.genders.map(gender => {
+                                return <option value={gender.value}>{gender.label}</option>
+                            })
+                        }
+                    </Field>
+                    <ErrorMessage name="gender" />
                     <br />
-                    <input className="btn draw-border" type="submit" value = "Register" />
-                </form>
-            </>
+
+                    <label htmlFor="username">Username</label>
+                    <Field name="username" />
+                    <ErrorMessage name="username" />
+                    <br />
+
+                    <label htmlFor="email">Email</label>
+                    <Field name="email" type="email" />
+                    <ErrorMessage name="email" />
+                    <br />
+
+                    <label htmlFor="password">Password</label>
+                    <Field name="password" type="password" />
+                    <ErrorMessage name="password" />
+                    <br />
+
+                    {/*TODO: Phone Input, Date picker and Address field*/}
+
+                    <label htmlFor="postcode">Postcode</label>
+                    <Field name="postcode" />
+                    <ErrorMessage name="postcode" />
+                    <br />
+
+                    <label htmlFor="studentStatus">Student Status</label>
+                    <Field name="studentStatus" as="select">
+                        <option defaultValue>Please Select...</option>
+                        {
+                            this.state.studentStatuses.map(studentStatus => {
+                                return <option value={studentStatus.value}>{studentStatus.label}</option>
+                            })
+                        }
+                    </Field>
+                    <ErrorMessage name="studentStatus" />
+                    <br />
+
+                    {/*TODO: number?*/}
+                    <label htmlFor="studentNumber">Student Number</label>
+                    <Field name="studentNumber" />
+                    <br />
+                </Form>
+            </Formik>
         )
     }
 }
