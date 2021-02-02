0e7625d55eaf75e9914ef4f37dfaa87a4f2d6013
Ray Clark <raynichc@users.noreply.github.com>
2021-02-02 17:47:49 +1100

    Added logged in redirect

--- src/src/pages/forgot-password.js
+++ src/src/pages/forgot-password.js
@@ -1,10 +1,18 @@
 import React from "react";
 
+import { navigate } from "gatsby"
+
 import PrimaryLayout from "../layouts/primaryLayout";
 import ForgotPassword from "../components/ForgotPassword";
 
+import { authenticationService } from "../services/authentication.js"
+
 //Basic Forgot Password page
 const FORGOT_PASSWORD = () => {
+    if (authenticationService.isLoggedIn()) {
+        navigate("/profile");   
+    }
+
     return (
         <PrimaryLayout>
         	<h1>Forgot Password</h1>
