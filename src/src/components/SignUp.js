a5f1023c9228aaf7e6155f9158346d34888c469c
Ray Clark <raynichc@users.noreply.github.com>
2021-02-02 17:34:40 +1100

    Add profile redirect and email message

--- src/src/components/SignUp.js
+++ src/src/components/SignUp.js
@@ -40,7 +40,7 @@ class SignUp extends React.Component {
         super(props);
 
         this.state = {
-            error: ""
+            registered: false
         }
     }
 
@@ -50,11 +50,6 @@ class SignUp extends React.Component {
         })
     }
 
-    //Function used to set form error
-    setError = (error) => {
-        this.setState({error: error});
-    }
-
     validationSchema = Yup.object().shape({
         nameFirst: Yup.string()
             .required("Required"),
@@ -62,7 +57,6 @@ class SignUp extends React.Component {
         nameLast: Yup.string()
             .required("Required"),
 
-        //TODO: Not working for some reason?
         sex: Yup.string()
             .ensure()
             .required("Required"),
@@ -141,7 +135,7 @@ class SignUp extends React.Component {
     }); 
 
     //Function to handle login form submission
-    handleSubmit = async (data) => {
+    handleSubmit = async (data, actions) => {
         //Attempt to register a new user
         //TODO: Add user details when registering
         const response = await authenticationService.register(data);
@@ -149,154 +143,180 @@ class SignUp extends React.Component {
         //Check register request was successful 
         if (response === true) {
             //TODO: Redirect to check email page or display check email message
+            this.setState({
+                registered: true
+            });
         } else {
-            this.setError(response);
+            actions.setFieldError('general', response);
         }
+
+        actions.setSubmitting(false);
     }
 
     //Simple Sign Up form
     render() {
         return (
+            !this.state.registered
+            ? (
             <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <Formik
-                initialValues={{
-                    nameFirst: "",
-                    dateOfBirth: new Date()
-                }}
-                validationSchema={this.validationSchema}
-                onSubmit={this.handleSubmit}
-            >
-                <Form>
-                    {/*TODO: Section "Your Details" */}
-                    <label htmlFor="nameFirst">First Name</label>
-                    <Field name="nameFirst" />
-                    <ErrorMessage name="nameFirst" />
-                    <br />
+                <Formik
+                    initialValues={{
+                        dateOfBirth: new Date()
+                    }}
+                    validationSchema={this.validationSchema}
+                    onSubmit={this.handleSubmit}
+                >
+                {(formProps) => (
+                    <Form>
+                        {/*TODO: Section "Your Details" */}
+                        <label htmlFor="nameFirst">First Name</label>
+                        <Field name="nameFirst" />
+                        <ErrorMessage name="nameFirst" />
+                        <br />
 
-                    <label htmlFor="nameLast">Last Name</label>
-                    <Field name="nameLast" />
-                    <ErrorMessage name="nameLast" />
-                    <br />
+                        <label htmlFor="nameLast">Last Name</label>
+                        <Field name="nameLast" />
+                        <ErrorMessage name="nameLast" />
+                        <br />
 
-                    <label htmlFor="sex">Sex</label>
-                    <Field name="sex" as="select">
-                        <option defaultValue>Please Select...</option>
-                        { this.mapOptions(this.sexes) }
-                    </Field>
-                    <ErrorMessage name="sex" />
-                    <br />
+                        <label htmlFor="sex">Sex</label>
+                        <Field name="sex" as="select">
+                            <option defaultValue>Please Select...</option>
+                            { this.mapOptions(this.sexes) }
+                        </Field>
+                        <ErrorMessage name="sex" />
+                        <br />
 
-                    <label htmlFor="username">Username</label>
-                    <Field name="username" />
-                    <ErrorMessage name="username" />
-                    <br />
+                        <label htmlFor="username">Username</label>
+                        <Field name="username" />
+                        <ErrorMessage name="username" />
+                        <br />
 
-                    <label htmlFor="email">Email</label>
-                    <Field name="email" type="email" />
-                    <ErrorMessage name="email" />
-                    <br />
+                        <label htmlFor="email">Email</label>
+                        <Field name="email" type="email" />
+                        <ErrorMessage name="email" />
+                        <br />
 
-                    <label htmlFor="password">Password</label>
-                    <Field name="password" type="password" />
-                    <ErrorMessage name="password" />
-                    <br />
+                        <label htmlFor="password">Password</label>
+                        <Field name="password" type="password" />
+                        <ErrorMessage name="password" />
+                        <br />
 
-                    {/*TODO: Change to PhoneInput*/}
-                    <label htmlFor="phoneNumber.number">Phone Number</label>
-                    <Field name="phoneNumber.number" />
-                    <ErrorMessage name="phoneNumber.number" />
+                        {/*TODO: Change to PhoneInput*/}
+                        <label htmlFor="phoneNumber.number">Phone Number</label>
+                        <Field name="phoneNumber.number" />
+                        <ErrorMessage name="phoneNumber.number" />
 
-                    <label htmlFor="phoneNumber.type">Type</label>
-                    <Field name="phoneNumber.type" as="select">                        
-                        <option defaultValue>Please Select...</option>
-                        { this.mapOptions(this.phoneTypes) }
-                    </Field>
-                    <ErrorMessage name="phoneNumber.type" />
-                    <br />
+                        <label htmlFor="phoneNumber.type">Type</label>
+                        <Field name="phoneNumber.type" as="select">                        
+                            <option defaultValue>Please Select...</option>
+                            { this.mapOptions(this.phoneTypes) }
+                        </Field>
+                        <ErrorMessage name="phoneNumber.type" />
+                        <br />
 
-                    <label htmlFor="dateOfBirth">Date of Birth</label>
-                    <Field name="dateOfBirth" component={DatePicker} />
-                    <ErrorMessage name="dateOfBirth" />
-                    <br />
+                        <label htmlFor="dateOfBirth">Date of Birth</label>
+                        <Field name="dateOfBirth" component={DatePicker} />
+                        <ErrorMessage name="dateOfBirth" />
+                        <br />
 
-                    {/* TODO: Look into google places API */}
-                    <label htmlFor="address.streetAddress">Address</label>
-                    <Field name="address.streetAddress" />
-                    <ErrorMessage name="address.streetAddress" />
-                    <br />
+                        {/* TODO: Look into google places API */}
+                        <label htmlFor="address.streetAddress">Address</label>
+                        <Field name="address.streetAddress" />
+                        <ErrorMessage name="address.streetAddress" />
+                        <br />
 
-                    <label htmlFor="address.postcode">Postcode</label>
-                    <Field name="address.postcode" />
-                    <ErrorMessage name="address.postcode" />
-                    <br />
+                        <label htmlFor="address.postcode">Postcode</label>
+                        <Field name="address.postcode" />
+                        <ErrorMessage name="address.postcode" />
+                        <br />
 
-                    <label htmlFor="studentStatus">Student Status</label>
-                    <Field name="studentStatus" as="select">
-                        <option defaultValue>Please Select...</option>
-                        { this.mapOptions(this.studentStatuses) }
-                    </Field>
-                    <ErrorMessage name="studentStatus" />
-                    <br />
+                        <label htmlFor="studentStatus">Student Status</label>
+                        <Field name="studentStatus" as="select">
+                            <option defaultValue>Please Select...</option>
+                            { this.mapOptions(this.studentStatuses) }
+                        </Field>
+                        <ErrorMessage name="studentStatus" />
+                        <br />
 
-                    {/*TODO: number?*/}
-                    <label htmlFor="studentNumber">Student Number</label>
-                    <Field name="studentNumber" />
-                    <ErrorMessage name="studentNumber" />
-                    <br />
+                        {/*TODO: number?*/}
+                        <label htmlFor="studentNumber">Student Number</label>
+                        <Field name="studentNumber" />
+                        <ErrorMessage name="studentNumber" />
+                        <br />
 
-                    {/*TODO: Section "Emergency Contact"*/}
-                    <label htmlFor="emergencyContact.name">Contact Name</label>
-                    <Field name="emergencyContact.name" />
-                    <ErrorMessage name="emergencyContact.name" />
-                    <br />
-                    
-                    {/*TODO: Change to PhoneInput*/}
-                    <label htmlFor="emergencyContact.phoneNumber.number">Phone Number</label>
-                    <Field name="emergencyContact.phoneNumber.number" />
-                    <ErrorMessage name="emergencyContact.phoneNumber.number" />
+                        {/*TODO: Section "Emergency Contact"*/}
+                        <label htmlFor="emergencyContact.name">Contact Name</label>
+                        <Field name="emergencyContact.name" />
+                        <ErrorMessage name="emergencyContact.name" />
+                        <br />
+                        
+                        {/*TODO: Change to PhoneInput*/}
+                        <label htmlFor="emergencyContact.phoneNumber.number">Phone Number</label>
+                        <Field name="emergencyContact.phoneNumber.number" />
+                        <ErrorMessage name="emergencyContact.phoneNumber.number" />
 
-                    <label htmlFor="emergencyContact.phoneNumber.type">Type</label>
-                    <Field name="emergencyContact.phoneNumber.type" as="select">                        
-                        <option defaultValue>Please Select...</option>
-                        { this.mapOptions(this.phoneTypes) }
-                    </Field>
-                    <ErrorMessage name="emergencyContact.phoneNumber.type" />
-                    <br />
+                        <label htmlFor="emergencyContact.phoneNumber.type">Type</label>
+                        <Field name="emergencyContact.phoneNumber.type" as="select">                        
+                            <option defaultValue>Please Select...</option>
+                            { this.mapOptions(this.phoneTypes) }
+                        </Field>
+                        <ErrorMessage name="emergencyContact.phoneNumber.type" />
+                        <br />
 
-                    {/* TODO: Look into google places API */}
-                    <label htmlFor="emergencyContact.address.streetAddress">Address</label>
-                    <Field name="emergencyContact.address.streetAddress" />
-                    <ErrorMessage name="emergencyContact.address.streetAddress" />
-                    <br />
+                        {/* TODO: Look into google places API */}
+                        <label htmlFor="emergencyContact.address.streetAddress">Address</label>
+                        <Field name="emergencyContact.address.streetAddress" />
+                        <ErrorMessage name="emergencyContact.address.streetAddress" />
+                        <br />
 
-                    <label htmlFor="emergencyContact.address.postcode">Postcode</label>
-                    <Field name="emergencyContact.address.postcode" />
-                    <ErrorMessage name="emergencyContact.address.postcode" />
-                    <br />
-                    {/*TODO: Section "Membership"*/}
+                        <label htmlFor="emergencyContact.address.postcode">Postcode</label>
+                        <Field name="emergencyContact.address.postcode" />
+                        <ErrorMessage name="emergencyContact.address.postcode" />
+                        <br />
+                        {/*TODO: Section "Membership"*/}
 
-                    {/*TODO: Get Waiver and Membership agreement from CMS*/}
-                    <label htmlFor="agreedLiabilityWaiver">Liability Waiver</label>
-                    <div>
-                        Waiver Lorem ipsum
-                    </div>
-                    <Field name="agreedLiabilityWaiver" type="checkbox" />
-                    <ErrorMessage name="agreedLiabilityWaiver" />
-                    <br />
+                        {/*TODO: Get Waiver and Membership agreement from CMS*/}
+                        <label htmlFor="agreedLiabilityWaiver">Liability Waiver</label>
+                        <div>
+                            Waiver Lorem ipsum
+                        </div>
+                        <Field name="agreedLiabilityWaiver" type="checkbox" />
+                        <ErrorMessage name="agreedLiabilityWaiver" />
+                        <br />
 
-                    <label htmlFor="agreedMembershipContract">Membership agreement</label>
-                    <div>
-                        Membership Lorem ipsum
-                    </div>
-                    <Field name="agreedMembershipContract" type="checkbox" />
-                    <ErrorMessage name="agreedMembershipContract" />
-                    <br />
+                        <label htmlFor="agreedMembershipContract">Membership agreement</label>
+                        <div>
+                            Membership Lorem ipsum
+                        </div>
+                        <Field name="agreedMembershipContract" type="checkbox" />
+                        <ErrorMessage name="agreedMembershipContract" />
+                        <br />
 
-                    <button type="submit">Sign Up!</button>
-                </Form>
-            </Formik>
+                        {   
+                            formProps.isSubmitting
+                            ? <LinearProgress />
+                            : <div style={{color: "red"}}>{formProps.errors.general}</div>
+                        }
+
+                        <Button
+                            className="btn draw-border"
+                            disabled={formProps.isSubmitting}
+                            onClick={formProps.handleSubmit}
+                        >
+                            Sign Up!
+                        </Button>
+                    </Form>
+                )}
+                </Formik>
             </MuiPickersUtilsProvider>
-        )
+            )
+            : (
+            <div>
+                Please check your email to confirm your account.
+            </div>
+            )
+        ) 
     }
 }
 
--- src/src/pages/sign-up.js
+++ src/src/pages/sign-up.js
@@ -1,13 +1,21 @@
 import React from "react";
 
+import { navigate } from "gatsby"
+
 import PrimaryLayout from "../layouts/primaryLayout";
 import SignUp from "../components/SignUp";
 
+import { authenticationService } from "../services/authentication.js"
+
 //Basic Sign Up page
 const SIGN_UP = () => {
+    if (authenticationService.isLoggedIn()) {
+        navigate("/profile");   
+    }
+
     return (
         <PrimaryLayout>
-        	<SignUp />
+            <SignUp />
         </PrimaryLayout>
     );
 };
