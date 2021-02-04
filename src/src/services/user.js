7b507bc052e6d0eb8766dbd034517c214d2110dc
Ray Clark <raynichc@users.noreply.github.com>
2021-02-04 15:58:18 +1100

    Fixed responses and migrated user select options

--- src/src/services/user.js
+++ src/src/services/user.js
@@ -2,8 +2,27 @@ import axios from "axios";
 
 import { authenticationService } from "../services/authentication.js"
 
-//TODO: Improve response and error handling
+const sexes = [
+    {label: "Male", value: "Male"},
+    {label: "Female", value: "Female"},
+    {label: "Not Stated", value: "Not_Stated"}
+]; 
 
+const phoneTypes = [
+    {label: "Mobile", value: "Mobile"},
+    {label: "Home", value: "Home"},
+    {label: "Work", value: "Work"}
+];
+
+const studentStatuses = [
+    {label: "Not UQ", value: "Not_UQ"},
+    {label: "Domestic", value: "Domestic"},
+    {label: "International", value: "International"},
+    {label: "Exchange", value: "Exchange"},
+    {label: "Staff", value: "Staff"}
+];
+
+//TODO: Improve response and error handling
 async function me() {
     const response = await axios.get(process.env.GATSBY_CMS_HOST + "/users/me", {
         headers: authenticationService.getAuthHeader()
@@ -24,9 +43,12 @@ async function find(id) {
     }).catch(error => {
         return false;
     });
+
+    return response;
 }
 
 async function update(id, data) {
+    console.log('update');
     const response = await axios.put(process.env.GATSBY_CMS_HOST + `/users/${id}`, data, {
         headers: authenticationService.getAuthHeader()
     }).then(response => {
@@ -39,18 +61,24 @@ async function update(id, data) {
 }
 
 async function updateMe(data) {
-    const response = await axios.put(process.env.GATSBY_CMS_HOST + "grampians/users/me", data, {
+    console.log('updateMe');
+    const response = await axios.put(process.env.GATSBY_CMS_HOST + "/grampians/users/me", data, {
         headers: authenticationService.getAuthHeader()
     }).then(response => {
         return true;
     }).catch(error => {
         return false;
     })
+
+    return response;
 }
 
 export const userService = {
     me,
     find,
     update,
-    updateMe
+    updateMe,
+    sexes,
+    phoneTypes,
+    studentStatuses
 };
