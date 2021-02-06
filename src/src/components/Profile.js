47e2c658a7df1853da0bb6c94482a6294f1c232c
Ray Clark <raynichc@users.noreply.github.com>
2021-02-06 14:10:48 +1100

    Implemented EmergencyContact component

--- src/src/components/Profile.js
+++ src/src/components/Profile.js
@@ -1,18 +1,11 @@
 import React from "react"
 
-import { Formik, Field, Form, ErrorMessage } from "formik"
-import * as Yup from "yup"
-import { Button, LinearProgress } from "@material-ui/core";
-import { DatePicker } from "formik-material-ui-pickers";
-import { MuiPickersUtilsProvider } from "@material-ui/pickers";
-import DateFnsUtils from "@date-io/date-fns";
-import Select from "../components/inputs/Select";
-
 import { userService } from "../services/user.js"
 
-import AccountDetails from "../components/profile/AccountDetails";
-import PersonalDetails from "../components/profile/PersonalDetails";
-import Tapes from "../components/profile/Tapes";
+import AccountDetails from "./profile/AccountDetails";
+import PersonalDetails from "./profile/PersonalDetails";
+import EmergencyContact from "./profile/EmergencyContact"
+import Tapes from "./profile/Tapes";
 
 //Basic User profile component
 class Profile extends React.Component {
@@ -37,75 +30,6 @@ class Profile extends React.Component {
         }
     }
 
-    validationSchema = Yup.object().shape({
-        nameFirst: Yup.string()
-            .required("Required"),
-
-        nameLast: Yup.string()
-            .required("Required"),
-
-        sex: Yup.string()
-            .ensure()
-            .required("Required"),
-
-        phoneNumber: Yup.object().shape({
-            number: Yup.string()
-                .required("Required"),
-
-            type: Yup.string()
-                .ensure()
-                .required("Required"),
-        }),
-
-        dateOfBirth: Yup.date()
-            .required("Required"),
-
-        address: Yup.object().shape({
-            streetAddress: Yup.string()
-                .required("Required"),
-
-            postcode: Yup.number()
-                .positive("Postcode must be greater than 0")
-                .integer("Postcode must be an integer")
-                .required("Required")
-        }),
-
-        studentStatus: Yup.string()
-            .ensure()
-            .required("Required"),
-
-        studentNumber: Yup.number()
-            .positive("Student Number must be positive")
-            .integer("Student Number must be an integer")
-            .required("Required"),
-
-        emergencyContact: Yup.object().shape({
-            name: Yup.string()
-                .required("Required"),
-
-            phoneNumber: Yup.object().shape({
-                number: Yup.string()
-                    .required("Required"),
-
-                type: Yup.string()
-                    .ensure()
-                    .required("Required"),
-            }),
-
-            address: Yup.object().shape({
-                streetAddress: Yup.string()
-                    .required("Required"),
-
-                postcode: Yup.number()
-                    .positive("Postcode must be greater than 0")
-                    .integer("Postcode must be an integer")
-                    .required("Required")
-            })
-        }),
-
-        medicalDetails: Yup.string(),
-    });
-
     //Simple Form with email and submit button
     render() {
         return (<>
@@ -118,41 +42,7 @@ class Profile extends React.Component {
             <PersonalDetails id={this.id} data={this.props.data} admin={this.state.admin} />
 
             <h2>Emergency Contact</h2>
-            <Formik
-                initialValues = {this.props.data}
-                validationSchema={this.validationSchema}
-                onSubmit={this.handleSubmit}
-            >
-            {(formProps) => (
-                <Form>  
-                    <label htmlFor="emergencyContact.name">Contact Name</label>
-                    <Field name="emergencyContact.name" disabled={!this.state.edit} />
-                    <ErrorMessage name="emergencyContact.name" />
-                    <br />
-                    
-                    {/*TODO: Change to PhoneInput*/}
-                    <label htmlFor="emergencyContact.phoneNumber.number">Phone Number</label>
-                    <Field name="emergencyContact.phoneNumber.number" disabled={!this.state.edit} />
-                    <ErrorMessage name="emergencyContact.phoneNumber.number" />
-
-                    <label htmlFor="emergencyContact.phoneNumber.type">Type</label>
-                    <Select name="emergencyContact.phoneNumber.type" options={userService.phoneTypes} disabled={!this.state.edit} /> 
-                    <ErrorMessage name="emergencyContact.phoneNumber.type" />
-                    <br />
-
-                    {/* TODO: Look into google places API */}
-                    <label htmlFor="emergencyContact.address.streetAddress">Address</label>
-                    <Field name="emergencyContact.address.streetAddress" disabled={!this.state.edit} />
-                    <ErrorMessage name="emergencyContact.address.streetAddress" />
-                    <br />
-
-                    <label htmlFor="emergencyContact.address.postcode">Postcode</label>
-                    <Field name="emergencyContact.address.postcode" disabled={!this.state.edit} />
-                    <ErrorMessage name="emergencyContact.address.postcode" />
-                    <br />
-                </Form>
-            )}
-            </Formik>
+            <EmergencyContact id={this.id} data={this.props.data} admin={this.state.admin} /> 
 
             <h2>Tapes</h2>
             <Tapes tapes={this.props.data.tapes} id={this.id} admin={this.state.admin} />
