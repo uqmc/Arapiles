c63aacf32e4f9e3d37ee8781613f3bccf5ee6187
Ray Clark <raynichc@users.noreply.github.com>
2021-02-07 09:53:15 +1100

    Fixed submit

--- src/src/components/profile/AccountDetails.js
+++ src/src/components/profile/AccountDetails.js
@@ -1,6 +1,6 @@
 import React from "react"
 
-import { Formik, Field, Form, ErrorMessage } from "formik"
+import { Formik, Form, ErrorMessage } from "formik"
 import * as Yup from "yup"
 import { Button } from '@material-ui/core';
 import Select from "../inputs/Select";
@@ -60,10 +60,7 @@ class AccountDetails extends React.Component {
                                 .ensure("Please select a Role.")
                                 .required("Required")
                         })}
-                        onSubmit={(data, actions) => {
-                            this.handleSubmit(data, actions);
-
-                        }}
+                        onSubmit={this.handleSubmit}
                     >
                     {(formProps) => (
                         <Form>
