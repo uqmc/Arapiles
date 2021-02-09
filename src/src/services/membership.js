ef064ba923796a9b450d2985080ed17396c68298
Ray Clark <raynichc@users.noreply.github.com>
2021-02-10 09:43:06 +1100

    Added membership services

---
+++ src/src/services/membership.js
@@ -0,0 +1,45 @@
+import axios from "axios";
+
+import { authenticationService } from "../services/authentication.js"
+
+import { add, parse, isFuture } from "date-fns";
+
+//TODO: Improve response and error handling
+async function memberships() {
+    const response = await axios.get(process.env.GATSBY_CMS_HOST + "/grampians/memberships", {
+        headers: authenticationService.getAuthHeader()
+    }).then(response => {
+        return response;
+    }).catch(error => {
+        return false;
+    })
+
+    return response;
+}
+
+async function pay(membershipID, token) {
+    const response = await axios.get(process.env.GATSBY_CMS_HOST + "/grampians/memberships", {
+        membershipID: membershipID,
+        token: token
+    }, {
+        headers: authenticationService.getAuthHeader()
+    }).then(response => {
+        return response;
+    }).catch(error => {
+        return false;
+    })
+
+    return response; 
+}
+
+function isValid(user) {
+    const currentMembershipStartDate = parse(user.currentMembershipStartDate, 'yyyy-MM-dd', new Date());
+    const currentMembershipEndDate = add(currentMembershipStartDate, { days: user.currentMembershipLength });
+    return isFuture(currentMembershipEndDate);
+}
+
+export const membershipService = {
+    memberships,
+    pay,
+    isValid
+};
