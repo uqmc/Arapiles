708086c2479e0a3e0cb712bcc3f1fc388218c137
Ray Clark <raynichc@users.noreply.github.com>
2021-02-11 17:30:36 +1100

    Added restrict component to payment

--- src/src/pages/payment.js
+++ src/src/pages/payment.js
@@ -4,6 +4,7 @@ import PrimaryLayout from "../layouts/primaryLayout";
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements, ElementsConsumer } from "@stripe/react-stripe-js";
 import Payment from "../components/Payment";
+import Restrict from "../components/Restrict";
 
 const stripePromise = loadStripe(process.env.GATSBY_STRIPE_KEY);
 
@@ -12,13 +13,15 @@ const PAYMENT = () => {
 
     return(
         <PrimaryLayout> 
-            <Elements stripe={stripePromise}>
-                <ElementsConsumer>
-                    {({stripe, elements}) => (
-                        <Payment stripe={stripe} elements={elements} />
-                    )}
-                </ElementsConsumer>
-            </Elements>
+            <Restrict>
+                <Elements stripe={stripePromise}>
+                    <ElementsConsumer>
+                        {({stripe, elements}) => (
+                            <Payment stripe={stripe} elements={elements} />
+                        )}
+                    </ElementsConsumer>
+                </Elements>
+            </Restrict>
         </PrimaryLayout>
     )
 };
