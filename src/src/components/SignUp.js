62faadf516193cc2fb92243799d275bdf0555fa7
Ray Clark <raynichc@users.noreply.github.com>
2021-02-15 10:57:18 +1100

    Reformatted signup output

--- src/src/components/SignUp.js
+++ src/src/components/SignUp.js
@@ -114,127 +114,126 @@ class SignUp extends React.Component {
 
     //Simple Sign Up form
     render() {
-        return (
-            !this.state.registered
-            ? (
-            <MuiPickersUtilsProvider utils={DateFnsUtils}>
-                <Formik
-                    initialValues={{
-                        dateOfBirth: new Date()
-                    }}
-                    validationSchema={this.validationSchema}
-                    onSubmit={this.handleSubmit}
-                >
-                {(formProps) => (
-                    <Form>
-                        <h2>Your Details</h2>
-                        <label htmlFor="nameFirst">First Name</label>
-                        <Field name="nameFirst" />
-                        <ErrorMessage name="nameFirst" />
-                        <br />
-
-                        <label htmlFor="nameLast">Last Name</label>
-                        <Field name="nameLast" />
-                        <ErrorMessage name="nameLast" />
-                        <br />
-
-                        <label htmlFor="sex">Sex</label>
-                        <Select name="sex" options={userService.sexes} />
-                        <ErrorMessage name="sex" />
-                        <br />
-
-                        <label htmlFor="username">Username</label>
-                        <Field name="username" />
-                        <ErrorMessage name="username" />
-                        <br />
-
-                        <label htmlFor="email">Email</label>
-                        <Field name="email" type="email" />
-                        <ErrorMessage name="email" />
-                        <br />
-
-                        <label htmlFor="password">Password</label>
-                        <Field name="password" type="password" />
-                        <ErrorMessage name="password" />
-                        <br />
-
-                        <Phone name="phoneNumber" />
-
-                        <label htmlFor="dateOfBirth">Date of Birth</label>
-                        <Field name="dateOfBirth" component={DatePicker} />
-                        <ErrorMessage name="dateOfBirth" />
-                        <br />
-
-                        <Address name="address" />
-
-                        <label htmlFor="studentStatus">Student Status</label>
-                        <Select name="studentStatus" options={userService.studentStatuses} />
-                        <ErrorMessage name="studentStatus" />
-                        <br />
-
-                        {/*TODO: number?*/}
-                        <label htmlFor="studentNumber">Student Number</label>
-                        <Field name="studentNumber" />
-                        <ErrorMessage name="studentNumber" />
-                        <br />
-
-                        <label htmlFor="medicalDetails">Medical Details</label>
-                        <ErrorMessage name="medicalDetails" />
-                        <br />
-                        <Field name="medicalDetails" as="textarea" />
-                        <br />
-
-                        <h2>Emergency Contact</h2>
-                        <label htmlFor="emergencyContact.name">Contact Name</label>
-                        <Field name="emergencyContact.name" />
-                        <ErrorMessage name="emergencyContact.name" />
-                        <br />
-                        
-                        <Phone name="emergencyContact.phoneNumber" />
-
-                        <Address name="emergencyContact.address" />
-
-                        <h2>Membership</h2>
-                        <label htmlFor="agreedLiabilityWaiver">Liability Waiver</label>
-                        <div>
-                            { this.state.waiver }
-                        </div>
-                        <Field name="agreedLiabilityWaiver" type="checkbox" />
-                        <ErrorMessage name="agreedLiabilityWaiver" />
-                        <br />
-
-                        <label htmlFor="agreedMembershipContract">Membership agreement</label>
-                        <div>
-                            { this.state.membership }
-                        </div>
-                        <Field name="agreedMembershipContract" type="checkbox" />
-                        <ErrorMessage name="agreedMembershipContract" />
-                        <br />
-
-                        {   
-                            formProps.isSubmitting
-                            ? <LinearProgress />
-                            : <div style={{color: "red"}}>{formProps.errors.general}</div>
-                        }
-
-                        <Button
-                            className="btn draw-border"
-                            disabled={formProps.isSubmitting}
-                            onClick={formProps.handleSubmit}
-                        >
-                            Sign Up!
-                        </Button>
-                    </Form>
-                )}
-                </Formik>
-            </MuiPickersUtilsProvider>
-            )
-            : (
-            <div>
+        if (this.state.registered) {
+            <p>
                 Please check your email to confirm your account.
-            </div>
-            )
-        ) 
+            </p>
+        } else {
+            return (
+                <MuiPickersUtilsProvider utils={DateFnsUtils}>
+                    <Formik
+                        initialValues={{
+                            dateOfBirth: new Date()
+                        }}
+                        validationSchema={this.validationSchema}
+                        onSubmit={this.handleSubmit}
+                    >
+                    {(formProps) => (
+                        <Form>
+                            <h2>Your Details</h2>
+                            <label htmlFor="nameFirst">First Name</label>
+                            <Field name="nameFirst" />
+                            <ErrorMessage name="nameFirst" />
+                            <br />
+
+                            <label htmlFor="nameLast">Last Name</label>
+                            <Field name="nameLast" />
+                            <ErrorMessage name="nameLast" />
+                            <br />
+
+                            <label htmlFor="sex">Sex</label>
+                            <Select name="sex" options={userService.sexes} />
+                            <ErrorMessage name="sex" />
+                            <br />
+
+                            <label htmlFor="username">Username</label>
+                            <Field name="username" />
+                            <ErrorMessage name="username" />
+                            <br />
+
+                            <label htmlFor="email">Email</label>
+                            <Field name="email" type="email" />
+                            <ErrorMessage name="email" />
+                            <br />
+
+                            <label htmlFor="password">Password</label>
+                            <Field name="password" type="password" />
+                            <ErrorMessage name="password" />
+                            <br />
+
+                            <Phone name="phoneNumber" />
+
+                            <label htmlFor="dateOfBirth">Date of Birth</label>
+                            <Field name="dateOfBirth" component={DatePicker} />
+                            <ErrorMessage name="dateOfBirth" />
+                            <br />
+
+                            <Address name="address" />
+
+                            <label htmlFor="studentStatus">Student Status</label>
+                            <Select name="studentStatus" options={userService.studentStatuses} />
+                            <ErrorMessage name="studentStatus" />
+                            <br />
+
+                            {/*TODO: number?*/}
+                            <label htmlFor="studentNumber">Student Number</label>
+                            <Field name="studentNumber" />
+                            <ErrorMessage name="studentNumber" />
+                            <br />
+
+                            <label htmlFor="medicalDetails">Medical Details</label>
+                            <ErrorMessage name="medicalDetails" />
+                            <br />
+                            <Field name="medicalDetails" as="textarea" />
+                            <br />
+
+                            <h2>Emergency Contact</h2>
+                            <label htmlFor="emergencyContact.name">Contact Name</label>
+                            <Field name="emergencyContact.name" />
+                            <ErrorMessage name="emergencyContact.name" />
+                            <br />
+                            
+                            <Phone name="emergencyContact.phoneNumber" />
+
+                            <Address name="emergencyContact.address" />
+
+                            <h2>Membership</h2>
+                            <label htmlFor="agreedLiabilityWaiver">Liability Waiver</label>
+                            <div>
+                                { this.state.waiver }
+                            </div>
+                            <Field name="agreedLiabilityWaiver" type="checkbox" />
+                            <ErrorMessage name="agreedLiabilityWaiver" />
+                            <br />
+
+                            <label htmlFor="agreedMembershipContract">Membership agreement</label>
+                            <div>
+                                { this.state.membership }
+                            </div>
+                            <Field name="agreedMembershipContract" type="checkbox" />
+                            <ErrorMessage name="agreedMembershipContract" />
+                            <br />
+
+                            {   
+                                formProps.isSubmitting
+                                ? <LinearProgress />
+                                : <div style={{color: "red"}}>{formProps.errors.general}</div>
+                            }
+
+                            <Button
+                                className="btn draw-border"
+                                disabled={formProps.isSubmitting}
+                                onClick={formProps.handleSubmit}
+                            >
+                                Sign Up!
+                            </Button>
+                        </Form>
+                    )}
+                    </Formik>
+                </MuiPickersUtilsProvider>
+                )
+            ) 
+        }
     }
 }
 
