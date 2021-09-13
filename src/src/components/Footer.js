74746af19e95e3f9efdcbcaaa26a38c289f1a2d8
vardy <jarred.vardy@gmail.com>
2021-09-13 17:27:41 +1000

    Add footer and fix link color

--- src/src/components/Footer.js
+++ src/src/components/Footer.js
@@ -2,7 +2,11 @@ import React from "react";
 
 const Footer = () => {
     return( 
-        <div style={{width: "100vw",height:"3em"}}></div>
+        <div className="footer">
+            <hr />
+            <p className="footer-text"><a href="mailto:uqmountainclub@gmail.com">uqmountainclub@gmail.com</a> <br /> ABN: 78 993 038 748</p>
+            <p className="footer-text">Copyright© 2021 UQMC <br /> Made with ❤️ by Jarred Vardy</p>
+        </div>
     );
 }
 
